46670368adbb596e75207fbebc4407cc0dc23a7d
fifi98 <bel.filip@hotmail.com>
2021-03-05 19:17:11 +0100

    Add MongoDB connection

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const User = require("./models/user");
 
-app.get("/", (req, res) => {
-  res.send("Test");
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+
+app.get("/", async (req, res) => {
+  try {
+    await new User().save();
+    const count = await User.countDocuments();
+    res.status(200).json({ success: true, count });
+  } catch (error) {
+    res.status(400).json({ success: false, error });
+  }
 });
 
 app.listen(process.env.PORT || 8080, () => console.log("Started!"));
